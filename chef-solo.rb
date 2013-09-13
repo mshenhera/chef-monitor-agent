e5ebbc2e36c4fe66496de6a1270b1a166080f552
Mykola Shengera <mshenhera@zorro.(none)>
2013-09-13 10:01:57 +0300

    Added chef-solo configuration files.

---
+++ chef-solo.rb
@@ -0,0 +1,5 @@
+cookbook_path [
+  "/etc/chef-solo/cookbooks",
+  "/etc/chef-solo/cookbooks-company"
+]
+log_level :info
---
+++ scripts/aws-bootstrap.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+EXIT_CODE=0
+
+# Install Chef\n",
+curl -L https://www.opscode.com/chef/install.sh | sudo bash || {
+  echo "Can't install Chef Client due to error above. Exiting."
+  exit 1
+}
+
+# install git
+apt-get update
+apt-get install -y git || {
+  echo "[ERROR] Exit due to error above."
+  exit 1
+}
+
+# Downloa chef-solo configuration.
+sudo git clone https://github.com/mshenhera/ChefSolo.git /etc/chef-solo
+
+# Setup Chef Client
+chef-solo -c /etc/chef-solo/chef-solo.rb -j /etc/chef-solo/default-node.json || {
+  echo "Failed to initialize host via chef solo"
+  exit 1
+}
+
+# Setup server using Chef\n",
+chef-client -j /etc/chef/first-boot.json || {
+  echo "Failed to initialize host via chef client" >> /var/log/bootstrap.log 2>&1
+  EXIT_CODE=1
+}
